bea4821a10ef7c0b5b5b8fe48da26d5117c51859
Andrey Sviontek <andrey.sviontek@gmail.com>
2023-11-19 22:18:47 +0500

    feat: add 2 tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -332,8 +332,11 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const arr = sentence.split(' ').sort((a, b) => {
+    return b.length - a.length;
+  });
+  return arr[0].toString();
 }
 
 /**
@@ -395,8 +398,8 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.slice(7, value.length - 1);
 }
 
 /**
@@ -410,8 +413,8 @@ function extractNameFromTemplate(/* value */) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.replace(/[^a-zа-яё0-9\s]/gi, '');
 }
 
 /**
